69095e15cd2aafb911f6bc914c3c3c3804601591
shajidislamc <shajidislamchowdhury@gmail.com>
2026-09-17 23:27:48 +0600

    Correct LLM name

--- agent_graph.py
+++ agent_graph.py
@@ -33,7 +33,7 @@ def get_llm(state: dict):
     
     api_key = state.get("groq_api_key") or os.getenv("GROQ_API_KEY")
     return ChatGroq(
-        model_name="llama-3.3-70b-versatile",
+        model_name="openai/gpt-oss-120b",
         groq_api_key=api_key,
         temperature=0.2
     )
